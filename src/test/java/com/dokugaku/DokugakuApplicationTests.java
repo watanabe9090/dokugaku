ef32d0e3b4a5c24f6658a990b3fe9ae0639b2a7e
Yuri Watanabe <yuri.watanabe@vilt.local>
2022-04-21 09:31:54 -0300

    Starting project

---
+++ src/test/java/com/dokugaku/DokugakuApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dokugaku;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokugakuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
