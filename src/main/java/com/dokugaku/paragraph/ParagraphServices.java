858787761e1336c186c4889dd91f8ac77fc73b98
Yuri Watanabe <yuri.watanabe@vilt.local>
2022-04-23 08:47:04 -0300

    EXEC: Paragraph creation when all parameters are OK

--- src/main/java/com/dokugaku/paragraph/ParagraphServices.java
+++ src/main/java/com/dokugaku/paragraph/ParagraphServices.java
@@ -1,4 +1,17 @@
 package com.dokugaku.paragraph;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ParagraphServices {
+
+    private final ParagraphRepository repository;
+
+    public Paragraph save(Paragraph paragraphToBeCreated) {
+        Paragraph paragraphSaved = repository.save(paragraphToBeCreated);
+        return paragraphSaved;
+    }
 }
--- src/test/java/com/dokugaku/paragraph/ParagraphServicesTest.java
+++ src/test/java/com/dokugaku/paragraph/ParagraphServicesTest.java
@@ -2,9 +2,11 @@ package com.dokugaku.paragraph;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ParagraphServicesTest {
 
     @Autowired
